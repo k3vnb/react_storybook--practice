8eec92f19727a73aecf4fabf3ddb119ed83184a8
Kevin Boyle <papershack@gmail.com>
2020-02-14 22:45:14 -0800

    add markup and fix syntax to Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-export default Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => (
-    <div className="list-item">
-        <input type="text" value={title} readOnly={true}/>
+const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => (
+    <div className={`list-item ${state}`}>
+        <label className="checkbox">
+            <input 
+                type="checkbox"
+                defaultChecked={state === 'TASK_ARCHIVED'}
+                disabled
+                name="checked"
+            />
+            <span className="checkbox-custom"/>
+        </label>
+        <div className="title">
+            <input type="text" value={title} readOnly placeholder="Input title" />
+        </div>
+        <div className="actions" onClick></div>
     </div>
 );
+
+export default Task;
